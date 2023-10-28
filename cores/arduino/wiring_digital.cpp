d8444620491f6318048377b7b02a0735561f96dc
shadow578 <52449218+shadow578@users.noreply.github.com>
2023-10-28 15:28:59 +0200

    Revert "slight refactor of `pinMode`"
    This reverts commit 13d6a295c6fb4cdabded350d60b0689d01ff9e4e.

--- cores/arduino/wiring_digital.cpp
+++ cores/arduino/wiring_digital.cpp
@@ -5,56 +5,21 @@
 #include "wiring_constants.h"
 #include "core_debug.h"
 
-/**
- * @brief set a pin to PWM mode
- */
-static void pin_set_pwm_mode(gpio_pin_t dwPin)
-{
-    // get timer assignment for pin
-    timera_config_t *unit;
-    en_timera_channel_t channel;
-    en_port_func_t port_function;
-    if (!timera_get_assignment(dwPin, unit, channel, port_function))
-    {
-        CORE_ASSERT_FAIL("analogWrite: pin is not a PWM pin");
-        return;
-    }
-
-    // initialize timer unit, allow incompatible config
-    switch (timera_pwm_start(unit, 1000 /* Hz */, 16, true))
-    {
-    case Ok:
-        // all good
-        break;
-    case ErrorOperationInProgress:
-        // already initialized with incompatible config
-        CORE_ASSERT_FAIL("timera_pwm_start failed: ErrorOperationInProgress");
-        return;
-    case ErrorInvalidParameter:
-    default:
-        // invalid parameter or other error
-        CORE_ASSERT_FAIL("timera_pwm_start failed");
-        return;
-    }
-
-    // initialize channel, start now
-    timera_pwm_channel_start(unit, channel, true);
-
-    // set pin function to TimerA output, no GPIO
-    GPIO_SetFunc(dwPin, port_function, Disable);
-}
-
 void pinMode(gpio_pin_t dwPin, uint32_t dwMode)
 {
-    ASSERT_GPIO_PIN_VALID(dwPin, "pinMode", return);
+    ASSERT_GPIO_PIN_VALID(dwPin, "pinMode");
+    if (dwPin >= BOARD_NR_GPIO_PINS)
+    {
+        return;
+    }
 
     // if pin has ADC channel, configure ADC according to pin mode
     pin_adc_info_t adc_info = PIN_MAP[dwPin].adc_info;
-    if (adc_info.has_adc())
+    adc_device_t *adc_device = adc_info.get_device();
+    uint8_t adc_channel = adc_info.channel;
+    if (adc_device != NULL && adc_channel != ADC_PIN_INVALID)
     {
-        adc_device_t *adc_device = adc_info.get_device();
-        uint8_t adc_channel = adc_info.channel;
-
+        // is a valid ADC pin
         if (dwMode == INPUT_ANALOG)
         {
             // initialize adc device (if already initialized, this will do nothing)
@@ -72,6 +37,7 @@ void pinMode(gpio_pin_t dwPin, uint32_t dwMode)
 
     // build pin configuration
     stc_port_init_t pinConf;
+    MEM_ZERO_STRUCT(pinConf);
     switch (dwMode)
     {
     case INPUT:
@@ -85,15 +51,46 @@ void pinMode(gpio_pin_t dwPin, uint32_t dwMode)
         pinConf.enPinMode = Pin_Mode_Ana;
         break;
     case OUTPUT_PWM:
-        pin_set_pwm_mode(dwPin);
+        // get timer assignment for pin
+        timera_config_t *unit;
+        en_timera_channel_t channel;
+        en_port_func_t port_function;
+        if (!timera_get_assignment(dwPin, unit, channel, port_function))
+        {
+            CORE_ASSERT_FAIL("analogWrite: pin is not a PWM pin");
+            return;
+        }
 
-        // return immediately, as pin_set_pwm_mode sets the pin function
+        // initialize timer unit, allow incompatible config
+        switch (timera_pwm_start(unit, 1000 /* Hz */, 16, true))
+        {
+        case Ok:
+            // all good
+            break;
+        case ErrorOperationInProgress:
+            // already initialized with incompatible config
+            CORE_ASSERT_FAIL("timera_pwm_start failed: ErrorOperationInProgress");
+            return;
+        case ErrorInvalidParameter:
+        default:
+            // invalid parameter or other error
+            CORE_ASSERT_FAIL("timera_pwm_start failed");
+            return;
+        }
+
+        // initialize channel, start now
+        timera_pwm_channel_start(unit, channel, true);
+
+        // set pin function to TimerA output, no GPIO
+        GPIO_SetFunc(dwPin, port_function, Disable);
+
+        // return immediately, as pwm needs different function
         return;
     case OUTPUT:
         pinConf.enPinMode = Pin_Mode_Out;
         break;
     default:
-        CORE_ASSERT_FAIL("pinMode: invalid pin mode. Must be INPUT, INPUT_PULLUP, INPUT_ANALOG, OUTPUT or OUTPUT_PWM");
+        CORE_ASSERT_FAIL("pinMode: invalid pin mode. Must be INPUT, INPUT_PULLUP, INPUT_ANALOG or OUTPUT");
         return;
     }
 
