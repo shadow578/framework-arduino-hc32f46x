5131da623b047e96a585a6858022674bb814e3b6
shadow578 <52449218+shadow578@users.noreply.github.com>
2024-03-01 08:48:55 +0100

    add core version macros

--- cores/arduino/core_util.h
+++ cores/arduino/core_util.h
@@ -4,3 +4,6 @@
 #define STRINGIFY_DETAIL(x) #x
 #define STRINGIFY(x) STRINGIFY_DETAIL(x)
 #endif
+
+#define GET_VERSION_INT(major, minor, patch) ((major * 100000) + (minor * 1000) + patch)
+#define ARDUINO_CORE_VERSION_INT GET_VERSION_INT(ARDUINO_CORE_MAJOR, ARDUINO_CORE_MINOR, ARDUINO_CORE_PATCH)
