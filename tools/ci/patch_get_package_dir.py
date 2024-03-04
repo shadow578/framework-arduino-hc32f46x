58e869f1a4f54eb8e17da7c66e1f134bbdad8b6b
shadow578 <52449218+shadow578@users.noreply.github.com>
2024-03-04 11:15:59 +0100

    update ci project structure

--- tools/ci/patch_get_package_dir.py
+++ tools/ci/patch_get_package_dir.py
@@ -2,9 +2,10 @@
 # does not apply to 'DefaultEnvironment().PioPlatform().get_package_dir()', which is used 
 # by the hc32f46x build system...
 #
-# this script is a workaround, patching the 'get_package_dir' method to return the project directory
-# for the 'framework-arduino-hc32f46x' package.
+# this script is a workaround, patching the 'get_package_dir' method to return the directory
+# defined by 'board_build.arduino_package_dir' (relative to $PROJECT_DIR) for the 'framework-arduino-hc32f46x' package.
 from SCons.Script import DefaultEnvironment
+from os.path import abspath, join
 
 env = DefaultEnvironment()
 platform = env.PioPlatform()
@@ -12,7 +13,14 @@ original_get_package_dir = platform.get_package_dir
 
 def get_package_dir_override(name):
     if name == "framework-arduino-hc32f46x":
-        return env.subst("$PROJECT_DIR")
+        project_dir = env.subst("$PROJECT_DIR")
+        arduino_package_dir = env.BoardConfig().get("build.arduino_package_dir", "")
+        if arduino_package_dir == "":
+            raise ValueError("board_build.arduino_package_dir is not defined")
+
+        package_dir = abspath(join(project_dir, arduino_package_dir))
+        print("Using package dir: " + package_dir)
+        return package_dir
     else:
         return original_get_package_dir(name)
 
