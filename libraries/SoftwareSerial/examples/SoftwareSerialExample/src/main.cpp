71b04cb002c94d7239d957a5753f8c511c117006
shadow578 <52449218+shadow578@users.noreply.github.com>
2025-02-08 11:53:05 +0100

    add dynamic baud change to softserial example

--- libraries/SoftwareSerial/examples/SoftwareSerialExample/src/main.cpp
+++ libraries/SoftwareSerial/examples/SoftwareSerialExample/src/main.cpp
@@ -2,6 +2,9 @@
  * Software serial basic example.
  *
  * echos any character received on the software serial port back to the sender.
+ * also allows to change the baud rate by sending a number from 1 to 8.
+ * 
+ * without changing the system clock, the maximum baud rate is around 9600 baud.
  */
 #include <Arduino.h>
 #include <SoftwareSerial.h>
@@ -20,7 +23,32 @@ void setup()
 void loop() 
 {
   while (mySerial.available())
-    mySerial.write(mySerial.read());
+  {
+    const char c = mySerial.read();
+    mySerial.write(c);
+
+    uint32_t new_baud = 0;
+    switch(c)
+    {
+      case '1': new_baud = 1200; break;
+      case '2': new_baud = 2400; break;
+      case '3': new_baud = 4800; break;
+      case '4': new_baud = 9600; break;
+      case '5': new_baud = 19200; break;
+      case '6': new_baud = 38400; break;
+      case '7': new_baud = 57600; break;
+      case '8': new_baud = 115200; break;
+      default: break;
+    }
+    if (new_baud != 0)
+    {
+      mySerial.print("Set baud to:");
+      mySerial.print(new_baud);
+      delay(100);
+      mySerial.begin(new_baud);
+      mySerial.println(" - Done");
+    }
+  }
 
   delay(10);
 }
